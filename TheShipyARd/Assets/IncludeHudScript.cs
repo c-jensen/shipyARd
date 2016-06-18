42ec6e94dbc7654e36687beaed8277e4fa5b105e
JanBTG <jan@a-beutgen.info>
2016-06-18 23:40:27 +0200

    Added basic AR Rendering Scene and UI elements

---
+++ TheShipyARd/Assets/IncludeHudScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncludeHudScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.LoadLevelAdditive("HudScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ TheShipyARd/Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameScript : MonoBehaviour {
+
+	// Update is called once per frame
+	public void ChangeToScene(int sceneToLoad) {
+        Application.LoadLevel(sceneToLoad);
+	}
+}
