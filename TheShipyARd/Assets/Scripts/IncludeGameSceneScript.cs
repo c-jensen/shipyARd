66cdafe5a3d7a387764e6ad967fc853e0fb16c7d
JanBTG <jan@a-beutgen.info>
2016-06-19 14:54:30 +0200

    Added server and room connection

---
+++ TheShipyARd/Assets/Scripts/IncludeGameSceneScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncludeGameSceneScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.LoadLevelAdditive("GameScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ TheShipyARd/Assets/Scripts/RandomMatchMaker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Photon;
+
+public class RandomMatchMaker : Photon.PunBehaviour
+{
+
+    void Start()
+    {
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join random room!");
+        PhotonNetwork.CreateRoom(null); //null could be any room name id
+    }
+}
