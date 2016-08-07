062229b4a15a3143fbb6c2359cd2c05d3141629d
JanBTG <jan@a-beutgen.info>
2016-08-07 13:40:54 +0200

    Added misssing files

---
+++ TheShipyARd/Assets/Scripts/NumOfPlayersScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumOfPlayersScript : MonoBehaviour {
+
+    private Slider slider;
+    private Text textBox;
+
+    // Use this for initialization
+    void Start () {
+        
+
+        GameObject sliderGO = GameObject.Find("NumOfPlayersSlider");
+        GameObject textBoxGO = GameObject.Find("NumOfPlayersIntDisplay");
+
+
+        if (sliderGO != null)
+        {
+            slider = sliderGO.GetComponent<Slider>();
+            slider.value = 2;
+        }
+
+        if (textBoxGO != null)
+        {
+            textBox = textBoxGO.GetComponent<Text>();
+            textBox.text = "2";
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        RandomMatchMaker.expectedNumberOfPlayers = (int) slider.value;
+        textBox.text = RandomMatchMaker.expectedNumberOfPlayers.ToString();
+    }
+}
