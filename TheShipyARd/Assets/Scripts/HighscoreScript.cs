2b7259a62d1e4ff3e8c2bd997d5db89e51412a4d
JanBTG <jan@a-beutgen.info>
2016-08-29 18:07:07 +0200

    added missing components

---
+++ TheShipyARd/Assets/Scripts/HighscoreScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class HighscoreScript : MonoBehaviour {
+
+    public bool firstRound;
+    public List<int> highscores;
+
+	// Use this for initialization
+	void Start () {
+        highscores = new List<int>();
+        firstRound = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
